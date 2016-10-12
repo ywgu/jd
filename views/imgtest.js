0736b3b0dfc5fe02d16204436f5da08fd935db13
William Gu <ywgu@yahoo.com>
2016-10-12 21:53:43 +0800

    test jimp

---
+++ views/imgtest.js
@@ -0,0 +1,19 @@
+/**
+ * Created by William Gu on 2016/9/8.
+ */
+var Jimp = require("jimp");
+
+// open a file called "lenna.png"
+Jimp.read("sample.jpg", function (err, lenna) {
+    if (err) throw err;
+    lenna
+        .resize(400,600)
+        // .brightness(0.2)
+        // .dither565()
+        // .contrast(0.6)
+        .greyscale()
+        .posterize(7.5)
+        .contrast(1)
+        // .blur(1)
+        .write("test.jpg"); // save
+});
