24b733a36aef49b11645d56c50dbd6522378f7fc
William Gu <ywgu@yahoo.com>
2016-10-22 12:06:09 +0800

    upload status and check image brightness

--- imgprocessor.js
+++ imgprocessor.js
@@ -24,21 +24,45 @@ process.on('message', function (imgInfo) {
     console.log('Image processing started...'+imgInfo);
     // var imgDest = imgInfo.substring(0,imgInfo.lastIndexOf('/')+1)+imgInfo.substring(imgInfo.lastIndexOf('/')+4);
     var imgDest = imgInfo.substring(0,imgInfo.lastIndexOf(".")) + ".bmp";
+    var darkness = 0;
+    var buffer;
     gm(imgInfo)
-        .resize(1050,788)   // 7 inch x 5.25 inch with 150 dpi
-        .modulate(120)  // brightness +20%
-        .contrast(3)
-        // .sharpen(3)
+        .resize(1,1)
         .colorspace("GRAY")
-        .dither(true)
-        .monochrome()
-        // .density(150,150)
-        .write(imgDest, function(err){
-                if (err) return console.dir(arguments);
-                console.log(this.outname + ' created :: ' + arguments[3]);
-                process.exit("DONE");
-            }
-        );
+        // .monochrome()
+        .toBuffer('TXT',function (err, buffer) {
+            if (err) return handle(err);
+            console.log('get darkness text:'+buffer);
+            var bufferStr = buffer.toString();
+            bufferStr = bufferStr.substring(bufferStr.indexOf(":")+1);
+            darkness = bufferStr.substring(bufferStr.indexOf("(")+1, bufferStr.indexOf(","));
+            console.log('darkness:'+darkness);
+            var brightness = 120;   // default add 20% brightness
+            // only make it brighter if it's dark
+            if (darkness < 80)
+                brightness += (128-darkness)/4;
+            // if (darkness > 156)  // too bright
+            //     brightness = 100-(darkness-156)/2;
+            // else if (darkness < 100)  // too dark
+            //     brightness = 100+(100-darkness)/2;
+            console.log("brightness:"+brightness);
+            gm(imgInfo)
+                .resize(1050,788)   // 7 inch x 5.25 inch with 150 dpi
+                .modulate(brightness)  // brightness +/-x%
+                .contrast(3)
+                // .sharpen(3)
+                .colorspace("GRAY")
+                .dither(true)
+                .monochrome()
+                // .density(150,150)
+                .write(imgDest, function(err){
+                        if (err) return console.dir(arguments);
+                        console.log(this.outname + ' created :: ' + arguments[3]);
+                        process.exit("DONE");
+                    }
+                );
+        })
+
     // jimp.read(imgInfo, function (err, img) {
     //     if (err) throw err;
     //     img
