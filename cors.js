f88fb57e8648088667ae1832b074eae748dce905
William Gu <ywgu@yahoo.com>
2017-01-02 13:59:18 -0600

    support cors

--- cors.js
+++ cors.js
@@ -1,7 +1,8 @@
 module.exports = function() {
     return function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
         next();
     };
 }
