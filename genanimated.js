e03d695667bba4e3f96bfbf884946be7ed5130e2
William Gu <ywgu@yahoo.com>
2017-02-26 23:44:40 +0800

    try ./templates instead of /designs/templates for rsvg within ImageMagick

--- genanimated.js
+++ genanimated.js
@@ -48,14 +48,14 @@ process.on('message', function (imgInfo) {
                     line = line.replace("\"/designs/temp/", "\"./temp/");
                 }
             }
-            // else if (line.indexOf("\"/design/templates/") > 0) {
-            //     if (isWin) {
-            //         line = line.replace("\"/design/", "\"" + dataDir + "/design/");
-            //     }
-            //     else { // for rsvg to embedded images can only be in subdirectories
-            //         line = line.replace("\"/design/templates/", "\"./templateimgs/");
-            //     }
-            // }
+            else if (line.indexOf("\"/designs/templates/") > 0) {
+                if (isWin) {
+                    line = line.replace("\"/design/", "\"" + dataDir + "/design/");
+                }
+                else { // for rsvg to embedded images can only be in subdirectories
+                    line = line.replace("\"/designs/templates/", "\"./templates/");
+                }
+            }
             // console.log(line);
             fs.appendFileSync(dest, line.toString() + "\n");
         });
