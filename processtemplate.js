3370d943ce5d3bb9df363d1b81e3dba6cdee1d01
William Gu <ywgu@yahoo.com>
2017-08-20 23:21:05 +0800

    transform templates using sax.

--- processtemplate.js
+++ processtemplate.js
@@ -1,7 +1,24 @@
+// 1. tag or attr
+// 2. D: delete; RA: ReplacePartialValue; SI: Special process for image tag; AF: Add Fonts
+// 3. attr=value
+// 4. new settings
+// var cmds = [
+//     "tag|D|id=jd_bd",
+//     "attr|RPV|id=text??|style=text-anchor:middle",
+//     "attr|RPV|id=text??|style=text-align:center",
+//     "tag|SI|id=image??",
+//     "tag|AF|<fonts>"
+// ];
 var sax = require('sax'),
     printer = sax.createStream(false, {lowercasetags: true, trim: true}),
     fs = require('fs');
 
+
+var inStream = null;
+var instructions = null;
+var outStream = null;
+
+// Some of the tag and attributes need to honor capital characters, otherwise it won't display right
 var tagMap = new Map([
     [ 'foreignobject', 'foreignObject' ],
     [ 'i:pgfref', 'i:pgfRef' ]
@@ -9,6 +26,26 @@ var tagMap = new Map([
 var attrMap = new Map([
     [ 'requiredextensions', 'requiredExtensions' ]
 ]);
+
+function replacePartialValue(orig, key, newVal) {
+    var splits = orig.split(";");
+    var result = "";
+    var needAppend = true;
+    for (var idx=0; idx<splits.length; idx++) {
+        var subKey = newVal.substring(0,newVal.indexOf(":"));
+        if (splits[idx].indexOf(subKey+":") === 0) {
+            result += newVal+";";
+            needAppend = false;
+        }
+        else {
+            result += splits[idx]+";";
+        }
+    }
+    if(needAppend) {
+        result += newVal;
+    }
+    return result;
+}
 
 function entity (str) {
     return str.replace('"', '&quot;')
@@ -24,10 +61,110 @@ printer.indent = function () {
         }
     }
 };
+// we don't support embedded tags yet
+var currentDeleteTag = null;
+var currentProcessTag = null;
 printer.on('opentag', function (tag) {
-    // if (tag.name === 'rect') {
-    //     return;
-    // }
+    for (var idx=0; idx<instructions.length; idx++) {
+        // processing tag|D command
+        var items = instructions[idx].split("|");
+        if (instructions[idx].indexOf("tag|D|") === 0) {
+            // mapping for attr=value
+            var filter = instructions[idx].substring(instructions[idx].lastIndexOf('|') + 1);
+            var name = filter.substring(0, filter.indexOf('='));
+            var value = filter.substring(filter.indexOf('=') + 1);
+            // console.log(name+","+value);
+            for (var j in tag.attributes) {
+                // console.log("=="+j+","+tag.attributes[j]);
+                if (j == name && tag.attributes[j] == value) {
+                    currentDeleteTag = tag.name;
+                    console.log("DELETED A LINE TAG:" + currentDeleteTag);
+                    return;
+                }
+            }
+        }
+        // end processing tag|D command
+        // processing attr|RPV|id=text??|style=text-anchor:middle command
+        if (instructions[idx].indexOf("attr|RPV|") === 0) {
+            // console.log(items);
+            var filterKey = items[2].substring(0, items[2].indexOf("="));
+            var filterVal = items[2].substring(items[2].indexOf("=") + 1);
+            // console.log(filterKey+","+filterVal);
+            var theVal = tag.attributes[filterKey];
+            // console.log(theVal);
+            if (theVal !== undefined && theVal !== null) {
+                var matches = theVal.match(filterVal);
+                if (matches && matches.length) {
+                    // !!! special check for textxx
+                    if ((theVal.indexOf("text") === 0 && theVal.length === 6)) {
+                        console.log("RPV:MATCH THE PATTERN:" + matches);
+                        var replaceAttrKey = items[3].substring(0, items[3].indexOf("="));
+                        var replaceAttrVal = items[3].substring(items[3].indexOf("=") + 1);
+                        // console.log("replaceAttrKey:"+replaceAttrKey+",replaceAttrVal:"+replaceAttrVal);
+                        tag.attributes[replaceAttrKey] = replacePartialValue(tag.attributes[replaceAttrKey], replaceAttrKey, replaceAttrVal);
+                        // console.log("new value:" + tag.attributes[replaceAttrKey]);
+                    }
+                }
+            }
+        }
+        // end processing attr|RPV|id=text??|style=text-anchor:middle command
+        // processing tag|SI|id=image[0-9][0-9] command
+        if (instructions[idx].indexOf("tag|SI|") === 0) {
+            var filterKey = items[2].substring(0, items[2].indexOf("="));
+            var filterVal = items[2].substring(items[2].indexOf("=") + 1);
+            // console.log(filterKey+","+filterVal);
+            var theVal = tag.attributes[filterKey];
+            if (theVal !== undefined && theVal !== null) {
+                var matches = theVal.match(filterVal);
+                if (matches && matches.length) {
+                    // !!! special check for imagexx
+                    if ((theVal.indexOf("image") === 0 && theVal.length === 7)) {
+                        console.log("PROCESSING IMAGE TAG");
+                        var newImgLine = "";
+                        var x = tag.attributes.x;
+                        var y = tag.attributes.y;
+                        var w = tag.attributes.width;
+                        var h = tag.attributes.height;
+                        var viewBox = "0 0 " + w + " " + h;
+                        // handle clip-path for image
+                        var clipPath = tag.attributes["clip-path"];
+                        if (clipPath !== null && clipPath !== undefined) {
+                            this.indent();
+                            print("\n<g clip-path=\"" + clipPath + "\" id=\"cp-layer\">");
+                        }
+                        // console.log("viewBox:"+viewBox);
+                        this.indent();
+                        print("<svg version=\"1.1\" viewBox=\"" + viewBox
+                            + "\" x=\"" + x + "\" y=\"" + y + "\" width=\"" + w + "\" height=\"" + h + "\" preserveAspectRatio=\"xMidYMid meet\">");
+                        tag.attributes.x = undefined;
+                        tag.attributes.y = undefined;
+                        tag.attributes.preserveaspectratio = undefined;
+                        if (clipPath !== null && clipPath !== undefined) {
+                            tag.attributes["clip-path"] = undefined;
+                        }
+                        var imgLine = "";
+                        this.indent();
+                        print('  <image');
+                        for (var i in tag.attributes) {
+                            if (tag.attributes[i] !== undefined)
+                                print(' ' + i + '="' + entity(tag.attributes[i]) + '"');
+                        }
+                        print('/>');
+                        this.indent();
+                        print("</svg>");
+                        if (clipPath !== null && clipPath !== undefined) {
+                            this.indent();
+                            print("</g>");
+                        }
+                        currentProcessTag = "image";
+                        return;
+                    }
+                }
+            }
+        }
+        // end processing tag|SI|id=image[0-9][0-9]
+    }
+
     this.indent();
     this.level++;
     var newName = tagMap.get(tag.name);
@@ -70,9 +207,18 @@ function ondoctypetext (text) {
     print("<!DOCTYPE "+text+">")
 }
 printer.on('closetag', function (tag) {
-    // if (tag === 'rect') {
-    //     return;
-    // }
+    // processing tag|D command
+    if (tag === currentDeleteTag) {
+        currentDeleteTag = null;
+        return;
+    }
+    // end processing tag|D command
+    // processing tag|SI|id=image[0-9][0-9]
+    if (tag === currentProcessTag) {
+        currentProcessTag = null;
+        return;
+    }
+    // end processing tag|SI|id=image[0-9][0-9]
     this.level--;
     this.indent();
     var newName = tagMap.get(tag);
@@ -111,21 +257,25 @@ function print (c) {
     }
 }
 
-var inStream = null;
-var instructions = null;
-var outStream = null;
 
 function processTemplateFile(inputFile, outputFile, instructs) {
-    instuctions = instructs;
+    instructions = instructs;
     inStream = fs.createReadStream(inputFile, { encoding: 'utf8' })
     outStream = fs.createWriteStream(outputFile);
     outStream.on('drain', function () {
         inStream.resume()
     });
     outStream.write("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+    // add fonts line if in the cmds
+    for (var i in instructs) {
+        if (instructs[i].indexOf("tag|AF|") === 0) {
+            var fontStr = instructs[i].substring(7);
+            outStream.write("\n<?xml-stylesheet type=\"text/css\" href=\"https://fonts.googleapis.com/css?family=" + fontStr.substring(0, fontStr.length - 1) + "\"?>");
+        }
+    }
     inStream.pipe(printer).pipe(fs.createWriteStream("nochange"));
 }
 
-processTemplateFile("0020001-2.svg","output.svg", "");
+// processTemplateFile("0020001-2.svg","output.svg", "");
 
-module.exports = {};
+module.exports = processTemplateFile;
---
+++ transformtemplate.js
@@ -0,0 +1,82 @@
+var processtemplate = require('./processtemplate.js');
+var fs = require("fs");
+var path = require('path');
+var publicDir = path.normalize(path.join(__dirname, '.', 'public'));
+var templateDir = path.join(publicDir, "designs/templates");
+var tempDir = path.join(publicDir, "designs/templates/designer/temp");
+
+var templateName;
+var templateNum;
+var templateIdx;
+var ignoreFontList = [ 'sans-serif', 'serif' ];
+var fontList = ["", "", "", ""];
+
+// use: node generatetemplate 0020001 3 22
+// generate from 0020001-0.svg 0020001-1.svg 0020001-2.svg to 002000122-0.svg 002000122-1.svg 002000122-2.svg + png, gif images
+process.argv.forEach(function (val, index, array) {
+    if (index === 2) {
+        templateName = val;
+    }
+    else if (index === 3) {
+        templateNum = val;
+    }
+    else if (index === 4) {
+        templateIdx = val;
+    }
+});
+console.log("args:" + templateName + "," + templateNum + "," + templateIdx);
+transform(templateName, templateNum, templateIdx);
+
+function getFontList(i, inputFile) {
+    var inputPath = tempDir + "/" + inputFile + ".svg";
+    var contents = fs.readFileSync(inputPath, 'utf8');
+    // console.log(contents);
+    var fontIdx = contents.indexOf("font-family:");
+    while (fontIdx > 0) {
+        // console.log(line);
+        var aFont = contents.substring(fontIdx + 12, contents.indexOf(";", fontIdx + 12));
+        if (aFont.indexOf('\'') === 0)
+            aFont = aFont.substring(1,aFont.length - 1);
+        aFont = aFont.replace(/ /g,"+");
+        if (ignoreFontList.indexOf(aFont) === -1)
+            aFont = aFont + "|";
+        else
+            aFont = "";
+        // console.log(fontList+","+aFont);
+        // console.log(fontList+aFont);
+        if (fontList[i].indexOf(aFont) < 0) {
+            fontList[i] = fontList[i].concat(aFont);
+            console.log("fontList " + i + ":" + fontList[i]);
+        }
+        fontIdx = contents.indexOf("font-family:", fontIdx+12);
+    }
+}
+
+// 1. tag or attr
+// 2. D: delete; RA: ReplacePartialValue; SI: Special process for image tag; AF: Add Fonts
+// 3. attr=value
+// 4. new settings
+var cmds = [
+    "tag|D|id=jd_bd",
+    "attr|RPV|id=text[0-9][0-9]|style=text-anchor:middle",
+    "attr|RPV|id=text[0-9][0-9]|style=text-align:center",
+    "tag|SI|id=image[0-9][0-9]",
+    "tag|AF|<fonts>"
+];
+function transform(templateName, templateNum, templateIdx) {
+    fontList = [];
+    for (var i = 0; i < templateNum; i++) {
+        // initialize variables
+        fontList[i] = "";
+        getFontList(i, templateName + "-" + i);
+        // processSVGImage(i, templateName + "-" + i, templateName + templateIdx + "--" + i);
+    }
+    // addToTemplateFile();    // NOTE: add a jd_bd before processing starts
+}
+
+// test call here
+for (var j in cmds) {
+    cmds[j] = cmds[j].replace("<fonts>",fontList[0]);
+    console.log("cmd:"+cmds[j]);
+}
+processtemplate("0020001-1.svg","output.svg", cmds);
